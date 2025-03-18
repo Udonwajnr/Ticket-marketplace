df9b2e55817cd2da67e12d0eea0c9377006ff07f
Umoh Udonwa <udonwajnr10@gmail.com>
2025-03-18 09:08:03 +0100

    commit

--- lib/baseUrl.ts
+++ lib/baseUrl.ts
@@ -1,7 +1,7 @@
-const baseUrl =
-  process.env.NODE_ENV === "development"
-    ? "http://localhost:3000/"
-    : `https://ticket-marketplace-six.vercel.app/`;
+const baseUrl = "https://ticket-marketplace-six.vercel.app/"
+//   process.env.NODE_ENV === "development"
+//     ? "http://localhost:3000/"
+//     : `https://ticket-marketplace-six.vercel.app/`;
 
 export default baseUrl;
 
