d5601d3a6fec3ed26c0dd5797a95aa0eb2cfeada
Umoh Udonwa <udonwajnr10@gmail.com>
2025-03-18 09:43:48 +0100

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import EventList from "@/components/EventList";
 import baseUrl from "@/lib/baseUrl";
 export default function Home() {
-  console.log(baseUrl)
+  console.log("Using Webhook Secret:", process.env.STRIPE_WEBHOOK_SECRET);
   return (
     <div>
         <EventList/>
