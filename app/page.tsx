12a34bc08e593b9fc22e5fa9814ac88670345e5e
Umoh Udonwa <udonwajnr10@gmail.com>
2025-03-18 09:50:46 +0100

    Trigger redeploy

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,12 @@ import Image from "next/image";
 import EventList from "@/components/EventList";
 import baseUrl from "@/lib/baseUrl";
 export default function Home() {
-  console.log("Using Webhook Secret:", process.env.STRIPE_WEBHOOK_SECRET);
+  // console.log("Using Webhook Secret:", process.env.STRIPE_WEBHOOK_SECRET);
+  if (!process.env.STRIPE_WEBHOOK_SECRET) {
+    console.error("🚨 STRIPE_WEBHOOK_SECRET is missing in production!");
+} else {
+    console.log("✅ STRIPE_WEBHOOK_SECRET is set:", process.env.STRIPE_WEBHOOK_SECRET);
+}
   return (
     <div>
         <EventList/>
