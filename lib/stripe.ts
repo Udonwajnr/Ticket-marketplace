7dc3bab2126d48232995abf74752c7c8596789ba
Umoh Udonwa <udonwajnr10@gmail.com>
2025-03-18 10:34:01 +0100

    commit

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -5,5 +5,5 @@ if(!process.env.STRIPE_SECRET_KEY){
 }
 
 export const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY, {
-    apiVersion: '2023-10-16'
+    apiVersion: '2025-02-24.acacia'
 });
