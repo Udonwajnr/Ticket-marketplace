f09ba6872158a988de88086844c329ecf42cc77f
Umoh Udonwa <udonwajnr10@gmail.com>
2025-03-18 10:27:49 +0100

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,13 @@ import Image from "next/image";
 import EventList from "@/components/EventList";
 import baseUrl from "@/lib/baseUrl";
 export default function Home() {
-  // console.log("Using Webhook Secret:", process.env.STRIPE_WEBHOOK_SECRET);
+
   if (!process.env.STRIPE_WEBHOOK_SECRET) {
     console.error("🚨 STRIPE_WEBHOOK_SECRET is missing in production!");
 } else {
     console.log("✅ STRIPE_WEBHOOK_SECRET is set:", process.env.STRIPE_WEBHOOK_SECRET);
 }
-console.log("hello")
+
   return (
     <div>
         <EventList/>
